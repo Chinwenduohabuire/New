53e20718ec182963d885bef517d25c4c92726aa5
Chinwe <chinwenduohabuire16@gmail.com>
2026-03-30 10:32:26 -0400

    boolean

--- src/main/java/Arithmetic.java
+++ src/main/java/Arithmetic.java
@@ -1,21 +1,33 @@
 public class Arithmetic {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 10;
+//        int a = 20;
+//        int b = 10;
+//
+//    int result = a + b;
+//        System.out.println(result);
+//
+//        boolean answer = a == b;
+//        System.out.println(answer);
+//
+//
+//        boolean c = false;
+//        boolean d = true;
+//
+//        boolean and = c && d;
+//        boolean or = c || d;
+//        boolean not = !c;
+//
+//        System.out.println(and);
+//        System.out.println(or);
+//        System.out.println(not);
 
-    int result = a + b;
-        System.out.println(result);
+        int a = 10;
+        int b = 8;
+        int c = 12;
 
-        boolean answer = a == b;
-        System.out.println(answer);
-
-
-        boolean c = false;
-        boolean d = true;
-
-        boolean and = c && d;
-        boolean or = c || d;
-        boolean not = !c;
+        boolean and = (a > b) && (b > c);
+        boolean or = (a > b) || (b > c);
+        boolean not = !(a > b);
 
         System.out.println(and);
         System.out.println(or);
