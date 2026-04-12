91e5e80ff19a3f7d7266d4bd18df5b223bc6bda2
Chinwe <chinwenduohabuire16@gmail.com>
2026-04-11 21:02:55 -0400

    oop

---
+++ src/main/java/Lesson.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Lesson {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public int plus(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Create a method with name "subtract" that subtracts two parameters
+    // of datatype double and returns double;
+    public double subtract(double c, double d) {
+        return c - d;
+    }
+
+    // Create a method with name "greeting" that concatenates two parameters
+    // of datatype string and returns string;
+    // run the tests with arguments "hello" and "world"
+    public String greeting(String first, String second) {
+        return first + second;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Lesson lesson = new Lesson();
+        System.out.println(lesson.subtract(1.5, 2.4));
+
+        System.out.println(lesson.greeting("hello ", "world"));
+        System.out.println(lesson.plus(2, 3, 4));
+    }
+
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,35 @@
+public class Test {
+    public static void main(String[] args) {
+
+        // instantiation
+        Lesson lesson = new Lesson();
+//        int c = lesson.add(2, 3);
+//        System.out.println(c);
+        System.out.println(lesson.add(2, 3));
+
+
+
+
+
+
+
+
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter first number: ");
+//        int a = scanner.nextInt();
+//
+//        System.out.println("Enter second number: ");
+//        int b = scanner.nextInt();
+//
+//        System.out.println(lesson.add(a, b));
+
+        // int a = 2;
+        // int b = 3;
+        // int c = a + b;
+        // System.out.println(a+b);
+        // System.out.println(c);
+    }
+}
